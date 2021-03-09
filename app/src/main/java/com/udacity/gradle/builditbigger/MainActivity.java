c3d879bae8be173ea85c30123c21c30c5a16f86f
nvrqt03 <ajktg21@gmail.com>
2021-03-09 13:44:52 -0600

    created first java library and joke

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import ajmitchell.android.javajokes.Jokes;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Jokes jokes = new Jokes();
+        Toast.makeText(this, jokes.tellJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ javajokes/src/main/java/ajmitchell/android/javajokes/Jokes.java
@@ -0,0 +1,9 @@
+package ajmitchell.android.javajokes;
+
+public class Jokes {
+
+    public String tellJoke() {
+        return "How many programmers does it take to change a lightbulb? \n" +
+                "None, that's a hardware problem";
+    }
+}
